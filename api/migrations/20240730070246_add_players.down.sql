d2516a6e34b62114c3ef73805fc16cde1fe6f788
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2024-07-30 10:19:37 +0200

    fix(migration)

--- api/migrations/20240730070246_add_players.down.sql
+++ api/migrations/20240730070246_add_players.down.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION get_online_players();
-DROP FUNCTION is_logged_in(uuid);
+DROP FUNCTION players.get_online_players();
+DROP FUNCTION players.is_logged_in(uuid);
 DROP TABLE players.logged_out;
 DROP TABLE players.logged_in;
 DROP TABLE players.player;
--- api/migrations/20240730070246_add_players.up.sql
+++ api/migrations/20240730070246_add_players.up.sql
@@ -7,33 +7,34 @@ CREATE TABLE players.player (
 );
 
 CREATE TABLE players.logged_in (
-    logged_in__id uuid primary key,
-    player__id uuid not null,
+    logged_in__id uuid primary key default public.gen_random_uuid(),
+    player_id uuid not null,
     created_at timestamptz not null default now(),
-    CONSTRAINT fk_logged_in__player__id FOREIGN KEY (player__id) REFERENCES players.player(player__id)
+    CONSTRAINT fk_logged_in__player__id FOREIGN KEY (player_id) REFERENCES players.player(player__id)
 );
+
 
 CREATE TABLE players.logged_out (
-    logged_out__id uuid primary key,
-    player__id uuid not null,
+    logged_out__id uuid primary key default public.gen_random_uuid(),
+    player_id uuid not null,
     created_at timestamptz not null default now(),
-    CONSTRAINT fk_logged_out__player__id FOREIGN KEY (player__id) REFERENCES players.player(player__id)
+    CONSTRAINT fk_logged_out__player__id FOREIGN KEY (player_id) REFERENCES players.player(player__id)
 );
 
-CREATE FUNCTION is_logged_in(player__id uuid) RETURNS boolean AS $$
+CREATE FUNCTION players.is_logged_in(player__id uuid) RETURNS boolean AS $$
 DECLARE
     last_login timestamptz;
     last_logout timestamptz;
 BEGIN
     SELECT created_at INTO last_login
     FROM players.logged_in
-    WHERE player__id = is_logged_in.player__id
+    WHERE player_id = player__id
     ORDER BY created_at DESC
     LIMIT 1;
 
     SELECT created_at INTO last_logout
     FROM players.logged_out
-    WHERE player__id = is_logged_in.player__id
+    WHERE player_id = player__id
     ORDER BY created_at DESC
     LIMIT 1;
 
@@ -47,8 +48,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION get_online_players() RETURNS TABLE (player__id uuid, name text) AS $$
+CREATE FUNCTION players.get_online_players() RETURNS TABLE (player__id uuid, name text) AS $$
     SELECT player__id, name
     FROM players.player
-    WHERE is_logged_in(player__id) = TRUE;
+    WHERE players.is_logged_in(player__id) = TRUE;
 $$ LANGUAGE sql;
